d79c877390ae6719a48904d404ed72c682a15bd6
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-28 15:12:18 +1000

    Updated library version

--- src/main/groovy/com.apiomega.build/services/impl/apiwar/ConfigureDependenciesImpl.groovy
+++ src/main/groovy/com.apiomega.build/services/impl/apiwar/ConfigureDependenciesImpl.groovy
@@ -7,14 +7,12 @@ import org.gradle.api.Project
  * Created by Matthew on 7/02/2016.
  */
 trait ConfigureDependenciesImpl implements ConfigureDependencies {
-    def apiOmegaVersion = "0.0.1-SNAPSHOT"
-
+    def apiOmegaVersion = "0.0.2-SNAPSHOT"
 
     void configureDependencies(Project project) {
         assert project != null;
 
         project.dependencies {
-
             compile 'com.apiomega:apiomega:' + apiOmegaVersion
         }
     }
