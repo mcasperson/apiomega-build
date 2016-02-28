e1c1d7a98c4b0d5e1a0b6b8911413eea843aa1a4
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-28 16:19:03 +1000

    Included swagger in the dependencies

--- src/main/groovy/com.apiomega.build/services/impl/apiwar/ConfigureDependenciesImpl.groovy
+++ src/main/groovy/com.apiomega.build/services/impl/apiwar/ConfigureDependenciesImpl.groovy
@@ -7,13 +7,15 @@ import org.gradle.api.Project
  * Defines the dependencies to be applied to WAR based API applications
  */
 trait ConfigureDependenciesImpl implements ConfigureDependencies {
-    def apiOmegaVersion = "0.0.4-SNAPSHOT"
+    def apiOmegaVersion = "0.0.5-SNAPSHOT";
+    def swaggerVersion = "1.5.0";
 
     void configureDependencies(Project project) {
         assert project != null;
 
         project.dependencies {
             compile 'com.apiomega:apiomega:' + apiOmegaVersion
+            compile 'io.swagger:swagger-jaxrs:' + swaggerVersion
         }
     }
 }
